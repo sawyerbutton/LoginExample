b3df3aad83c663f2b0c72164b092d8d551403f0d
sawyerbutton <lil103@pitt.edu>
2018-03-27 15:01:34 -0400

    still dont know how to connect the json server with current front end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { routing } from './app.routing';
+import { AuthGuard } from './guards/auth.guard';
 import { AppComponent } from './app.component';
 import { AlertInfoComponent } from './alert-info/alert-info.component';
-
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AlertService, AuthenticationService, UserService } from './services/index'
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlertInfoComponent
+    AlertInfoComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    AuthGuard,
+    AlertService,
+    AuthenticationService,
+    UserService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { User} from '../models/user';
+import { UserService} from '../services/user.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  public currentUser: User;
+  public users: User[] = [];
+
+  constructor(private userService: UserService) {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
+
+  ngOnInit() {
+    this.loadAllUsers();
+  }
+
+  deleteUser(id: number) {
+    this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
+  }
+
+  private loadAllUsers() {
+    this.userService.getAll().subscribe(users => { this.users = users; });
+  }
+
+}
